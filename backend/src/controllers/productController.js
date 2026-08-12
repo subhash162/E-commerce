1c2db106a78b2dc597fa7d44d16922afc4a21b94
Subhash Thakur <104288600+subhash162@users.noreply.github.com>
2026-08-12 18:34:49 +0545

    feat: completed product CRUD API

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,3 +1,4 @@
+import { ExplainableCursor } from "mongodb";
 import pool from "../config/db.js";
 
 export async function getProducts(req,res){
@@ -53,4 +54,59 @@ export async function createProduct(req,res) {
             error:error.message,
         })
     }
+}
+export async function updateProduct(req,res){
+
+    try{
+        const id=Number(req.params.id);
+    const { name , price , category }=req.body;
+    if(!name || !price || !category ){
+        res.status(400).json({
+            message:"Name Price and Category are required",
+        })
+    }
+    const result=await pool.query(
+        `UPDATE products
+       SET name = $1,
+           price = $2,
+           category = $3
+       WHERE id = $4
+       RETURNING *`,
+       [ name , price , category , id ]
+    ) 
+    if(result.rows.length===0){
+        return res.status(404).json({
+            message:"Product not found"
+        })
+    }
+     res.status(200).json(result.rows[0])
+    }catch(error){
+        res.status(500).json({
+            message:"failed to update product"
+        })
+    }
+    
+}
+export async function deleteProduct(req, res) {
+  try {
+    const id = Number(req.params.id);
+
+    const result = await pool.query(
+      `DELETE FROM products
+       WHERE id = $1
+       RETURNING *`,
+      [id]
+    );
+
+    res.json({
+      message: "Product deleted successfully",
+      product: result.rows[0],
+    });
+  } catch (error) {
+    console.error("DELETE PRODUCT ERROR:", error);
+
+    res.status(500).json({
+      message: "Failed to delete product",
+    });
+  }
 }
--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -3,10 +3,14 @@ import express from 'express';
 import { 
     getProducts , 
     getProductById,
-    createProduct
+    createProduct,
+    updateProduct,
+    deleteProduct
  } from '../controllers/productController.js';
 const router=express.Router();
 router.get("/",getProducts);
 router.get("/:id",getProductById);
 router.post("/",createProduct);
+router.put("/:id",updateProduct);
+router.delete("/:id",deleteProduct);
 export default router;
